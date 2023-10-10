4114e05db536fcb708d18cd0fe416ab69e5c8c9a
itzRulzz <enotokot.001@gmail.com>
2023-10-11 00:30:25 +0300

    feat: added logo in the center, configured static files, added associated FT

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
+import requests
 
 class MainTest(unittest.TestCase):
 
@@ -21,16 +22,21 @@ class MainTest(unittest.TestCase):
         self.assertIn('рецепты', self.browser.title)
 
         # Коля обращает внимание на логотип, красующийся по центру страницы.
+        logo = self.browser.find_element('id', 'id_logo')
+        logo_link = logo.get_attribute('src')
+        req = requests.get(logo_link)
+        self.assertEqual(req.status_code, 200)
 
         # Коля также подмечает приятный сероватый фон сайта.
 
         # Коля видит строку для ввода запроса.
 
         # В строке ввода написано: "Введите название блюда..."
+        self.fail('Finished!')
 
-    def test_can_retrieve_recipes(self):
+    #def test_can_retrieve_recipes(self):
         # Он вводит "Парфе" в текст-бокс (Коля любит французскую кухню).
-        pass
+        
 
         # Коля нажимает Enter, ему выводится список рецептов этого блюда с разных сайтов.
 
