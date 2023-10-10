4ee4f30e1662bac33af99444c7a988eee962dd07
itzRulzz <enotokot.001@gmail.com>
2023-10-10 21:16:17 +0300

    feat!: refactored unit-tests, view, added first template - home.html

--- parfait_base/tests.py
+++ parfait_base/tests.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
-from django.urls import resolve
-from parfait_base.views import home_page
 
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+    
