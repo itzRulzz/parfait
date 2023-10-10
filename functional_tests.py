700ca907fd97e8950dd90fec3b0e8a26145f0730
itzRulzz <enotokot.001@gmail.com>
2023-10-11 00:52:55 +0300

    feat: added FTs to check the UI, started writing the FT to check the retrievement of recipes

--- functional_tests.py
+++ functional_tests.py
@@ -11,7 +11,7 @@ class MainTest(unittest.TestCase):
 
     # Удовлетворенный, Коля уходит спать.
     def tearDown(self):
-        self.browser.quit
+        self.browser.quit()
 
     def test_can_load_a_page_correctly(self):
         # Коля услышал о веб-приложении, которое помогает не тратить время
@@ -28,17 +28,27 @@ class MainTest(unittest.TestCase):
         self.assertEqual(req.status_code, 200)
 
         # Коля также подмечает приятный сероватый фон сайта.
+        body = self.browser.find_element('tag name', 'body')
+        body_style = body.get_attribute('style')
+        self.assertIn(body_style, 'background-color: rgb(246, 246, 246);')
 
         # Коля видит строку для ввода запроса.
+        search_bar = self.browser.find_element('id', 'id_new_search')
+        self.assertNotEqual(search_bar, None)
 
         # В строке ввода написано: "Введите название блюда..."
-        self.fail('Finished!')
+        search_bar_text = search_bar.get_attribute('placeholder')
+        self.assertEqual(search_bar_text, 'Введите название блюда...')
 
-    #def test_can_retrieve_recipes(self):
+    def test_can_retrieve_recipes(self):
         # Он вводит "Парфе" в текст-бокс (Коля любит французскую кухню).
-        
+        search_bar = self.browser.find_element('id', 'id_new_search')
+        search_bar.send_keys('Парфе')
 
         # Коля нажимает Enter, ему выводится список рецептов этого блюда с разных сайтов.
+        search_bar.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
 
         # Коле интересно, сбросятся ли найденные ссылки при обновлении страницы.
         # Коля замечает, что для него сгенерирован персональный URL.
